7c2382fe280a615e400039f3a5ae78d66f508c77
Tina <thywang1@gmail.com>
2021-12-29 20:12:46 -0500

    Calculated button positions and drew buttons

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ WIDTH, HEIGHT = 800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game!")
 
+# button variables
+RADIUS = 20
+GAP = 15
+letters = []
+startx = round((WIDTH - (RADIUS * 2 + GAP) * 13) / 2)
+starty = 400
+# determine position of each button
+for i in range(26):
+    # two rows of buttons
+    x = startx + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
+    y = starty + ((i // 13) * (GAP + RADIUS * 2))
+    letters.append([x, y])
+
+print(letters)
+
 # load images
 images = []
 for i in range(7):
@@ -18,6 +33,7 @@ hangman_status = 4
 
 # colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # set up game loop
 # define max FPS: 60 frames per second
@@ -27,17 +43,30 @@ clock = pygame.time.Clock()
 # controls the game loop
 run = True
 
+# function to draw
+
+
+def draw():
+    # window background
+    win.fill(WHITE)
+    # draw buttons
+    for letter in letters:
+        # unpacking
+        x, y = letter
+        # where, what color, position of centre, radius, thickness
+        pygame.draw.circle(win, BLACK, (x, y), RADIUS, 3)
+    # draw hangman image
+    win.blit(images[hangman_status], (150, 100))
+    # update display
+    pygame.display.update()
+
+
 # the game loop
 while run:
     # to make sure our loop runs at predefined speed
     clock.tick(FPS)
 
-    # window background
-    win.fill(WHITE)
-    # draw hangman image
-    win.blit(images[hangman_status], (150, 100))
-    # update display
-    pygame.display.update()
+    draw()
 
     # check for mouseclick events
     # any event that happens will be stored in pygame.event.get()
